79e61b5e38a873365f3321b06f21272668198a6f
Xavier Roumegue <xroumegue@gmail.com>
2017-04-28 17:49:23 +0200

    bin: ipm: First IPM opengl implementation working
    Signed-off-by: Xavier Roumegue <xroumegue@gmail.com>

--- bin/ipm.frag
+++ bin/ipm.frag
@@ -1,80 +1,72 @@
 #version 450
 
-uniform mat2 wROI;
-uniform mat2 iROI;
-uniform vec2 iResolution;
-uniform sampler2D tex;
+uniform vec3      	iResolution;
+uniform sampler2D 	iChannel;
+uniform vec3      	iChannelResolution;
+uniform mat4		uMg2i;
+uniform mat4		uMi2g;
+uniform mat2		uwROI;
+uniform mat2		uIPM;
+uniform float		uH;
 
-uniform float pitch;
-uniform float yaw;
-uniform float fu;
-uniform float fv;
-uniform float cu;
-uniform float cv;
-uniform float h;
+vec2 Ti2g(in vec2 _in)
+{
+        vec4 g;
+        vec2 g2ret;
+        vec4 _in4;
+        _in4.xy = _in.xy;
+        _in4.z = 1.0;
+        _in4.w = 1.0;
 
-in vec4 fragCoord;
-out vec4 fragColor;
+        g = transpose(uMi2g) * _in4;
+        g2ret.xy = g.xy;
+        g2ret.xy /= g.w;
+
+        return g2ret;
+}
 
 vec2 Tg2i(in vec2 _in)
 {
+        vec2 i2ret;
+        vec4 _in4;
+        vec4 i;
 
-float c1 = cos(pitch);
-float s1 = sin(pitch);
-float c2 = cos(yaw);
-float s2 = sin(yaw);
+        _in4.xy = _in.xy;
+        _in4.z = -uH;
+        _in4.w = 1.0;
 
-    vec2 i2ret;
-    vec4 _in4;
-    vec4 i;
+        i = transpose(uMg2i) * _in4;
+        i2ret.xy = i.xy / i.z;
 
-	_in4.xy = _in.xy;
-    _in4.z = -h;
-    _in4.w = 1.0;
-
-    mat4 Mg2i = transpose(
-        			mat4(
-                        	fu*c2 + cu*c1*s2,	cu*c1*c2 - s2*fu,	-cu*s1,				0,
-                        	s2*(cv*c1 - fv*s1),	c2*(cv*c1 - fv*s1),	-fv*c1 - cv*s1,		0,
-							c1*s2,				c1*c2,				-s1,				0,
-							c1*s2,				c1*c2,				-s1,				0
-                    )
-        		);
-
-    i = Mg2i * _in4;
-    i2ret.xy = i.xy / i.z;
-
-    return i2ret;
-
+        return i2ret;
 }
 
 void main()
 {
-	vec2 uv = fragCoord.xy / iResolution.xy;
+        vec2 uv = gl_FragCoord.xy / iResolution.xy;
+        float xMin = uwROI[0].x;
+        float xMax = uwROI[1].x;
+        float yMin = uwROI[0].y;
+        float yMax = uwROI[1].y;
 
-    float xMin = wROI[0].x;
-    float xMax = wROI[1].x;
-    float yMin = wROI[0].y;
-    float yMax = wROI[1].y;
+        float yScale = yMax - yMin;
+        float xScale = xMax - xMin;
 
-    float yScale = yMax - yMin;
-    float xScale = xMax - xMin;
+        vec2 w = vec2(xMin + uv.x * xScale, yMin + uv.y * yScale);
+        vec2 i = Tg2i(w);
 
-    vec2 w = vec2(xMin + uv.x * xScale, yMin + uv.y * yScale);
-	vec2 i = Tg2i(w);
+        vec4 outOfROI = vec4(0.0, 0.0, 0.0, 1.0);
 
-	vec4 outOfROI = vec4(0.0, 0.0, 1.0, 1.0);
-
-    if (i.x < iROI[0].x)
-    	fragColor = outOfROI;
-    else if (i.x > iROI[1].x)
-        fragColor = outOfROI;
-	else if (i.y > iROI[1].y)
-        fragColor = outOfROI;
-	else if (i.y < iROI[0].y)
-        fragColor = outOfROI;
-    else
-        i.x /= iResolution.x;
-        i.y /= iResolution.y;
-		fragColor = texture(tex, i);
+        if (i.x < uIPM[0].x)
+                gl_FragColor = outOfROI;
+        else if (i.x > uIPM[1].x)
+                gl_FragColor = outOfROI;
+        else if (i.y > uIPM[0].y)
+                gl_FragColor = outOfROI;
+        else if (i.y < uIPM[1].y)
+                gl_FragColor = outOfROI;
+        else {
+                i.xy /= iChannelResolution.xy;
+                gl_FragColor = texture(iChannel, i);
+        }
 }
--- bin/ipm.py
+++ bin/ipm.py
@@ -2,263 +2,226 @@
 
 import sys
 try:
-	from OpenGL.GL import *
-	from OpenGL.GLUT import *
-	from OpenGL.GLU import *
-	from OpenGL.GL.ARB.shader_objects import *
-	from OpenGL.GL.ARB.fragment_shader import *
-	from OpenGL.GL.ARB.vertex_shader import *
+    import numpy as np
 except:
-	print("Error importing GL / shaders")
-	sys.exit()
+    print("Error while importing numpy, please do: # pip install numpy")
+    sys.exit()
 
+try:
+    import vispy
+    from vispy import app
+    from vispy import gloo
+except:
+    print("Error while importing vispy module, please do: #pip install vispy")
+    sys.exit()
+
+try:
+    from PIL import Image
+except:
+    print("Error while importing PIL, please do: #pip install Pillow")
+    sys.exit()
+
+from os import listdir
 from os.path import isfile, dirname, realpath, join
-from PIL import Image
-import logging
-import numpy as np
-import math
 
+customLibPath=join(dirname(realpath(__file__)),'../lib/python')
+for _dir in listdir(customLibPath):
+    sys.path.append(dirname(realpath(join(customLibPath, _dir))))
+
+import laneDetector.ipm as ipm
+import logging
+import math
 from argparse import ArgumentParser, FileType, Action, Namespace
+import configparser
 
 FORMAT = '%(asctime)-15s-%(levelname)-5s-%(funcName)-8s-%(lineno)-4s-%(message)s'
 
-ESCAPE = '\033'
+FRAGMENT_SHADER_FILENAME = join(dirname(realpath(__file__)), 'ipm.frag')
+VERTEX_SHADER_FILENAME = join(dirname(realpath(__file__)), 'ipm.vert')
+
+class IpmCanvas(app.Canvas, ipm):
+    def __init__(self, fileName, conf):
+        app.Canvas.__init__(self,
+                        keys = 'interactive',
+                        size = (1280, 720),
+                        position = (0,0),
+                        title = 'IPM',
+                        show = False,
+                        resizable = True)
+
+        ipm.__init__(self, conf, "ipm openGL")
+
+        try:
+            fragmentShaderSourceString = open(FRAGMENT_SHADER_FILENAME).read()
+        except:
+            self.logger.fatal("%s does not exist !", FRAGMENT_SHADER_FILENAME)
+            sys.exit()
+
+        try:
+            vertexShaderSourceString = open(VERTEX_SHADER_FILENAME).read()
+        except:
+            self.logger.fatal("%s does not exist !", VERTEX_SHADER_FILENAME)
+            sys.exit()
+
+        self.program = gloo.Program(vertexShaderSourceString, fragmentShaderSourceString)
+        self.program["position"] = [(-1, -1), (-1, 1), (1, 1), (-1, -1), (1, 1), (1, -1)]
+
+        img = Image.open(fileName)
+        im = np.array(list(img.getdata()),np.uint8).reshape((img.size[1], img.size[0], 3))
+
+        gloo.set_viewport(0, 0, *self.physical_size)
+
+        tex = gloo.Texture2D(im)
+        tex.interpolation = 'linear'
+        tex.wrapping = 'repeat'
+        self.program['iChannel'] = tex
+        self.program['iChannelResolution'] = (im.shape[1], im.shape[0], im.shape[2])
+        self.program['iResolution'] = (self.physical_size[0], self.physical_size[1], 0.)
+
+        self.getUniforms()
+        self.show()
+
+    def getUniforms(self):
+        self._getMi2gMat()
+        self._getMg2iMat()
+        self._getUniformROI()
+        self._getUniformIPM()
+        self.program['uH'] = self.conf['h']
+
+    def _getUniformROI(self):
+        _ROI = self.getROI()
+        uROI = np.array((2,2), dtype=np.float32)
+        uROI = [
+            (_ROI['x']['min'], _ROI['y']['min']),
+            (_ROI['x']['max'], _ROI['y']['max'])
+        ]
+        self.program['uwROI'] = uROI
+        return uROI
+
+    def _getUniformIPM(self):
+        uIPM = np.array((2,2), dtype=np.float32)
+        uIPM = [
+            (self.conf['ipmLeft'], self.conf['ipmBottom']),
+            (self.conf['ipmRight'], self.conf['ipmTop'])
+
+        ]
+        self.program['uIPM'] = uIPM
+        return uIPM
+
+    def _getMi2gMat(self):
+        c1 = self.conf['c1']
+        s1 = self.conf['s1']
+        c2 = self.conf['c2']
+        s2 = self.conf['s2']
+        fu = self.conf['fu']
+        fv = self.conf['fv']
+        cu = self.conf['cu']
+        cv = self.conf['cv']
+        h = self.conf['h']
+
+        Mi2g = np.zeros((4, 4), dtype=np.float32)
+        Mi2g = [
+                (-c2/fu, s1*s2/fv,       (cu*c2/fu) - (cv*s1*s2/fv) - (c1*s2),   0),
+                (s2/fu,  s1*c1/fv,       (-cu*s2/fu) - (cv*s1*c2/fv) - (c1*c2),  0),
+                (0,      c1/fv,          (-cv*c1/fv) +s1,                        0),
+                (0,      -c1/(h*fv),     (cv*c1/(h*fv)) - (s1/h),                0)
+        ]
+
+        self.program['uMi2g'] = Mi2g
+        return Mi2g
+
+
+    def _getMg2iMat(self):
+        c1 = self.conf['c1']
+        s1 = self.conf['s1']
+        c2 = self.conf['c2']
+        s2 = self.conf['s2']
+        fu = self.conf['fu']
+        fv = self.conf['fv']
+        cu = self.conf['cu']
+        cv = self.conf['cv']
+        h = self.conf['h']
+
+        Mg2i = np.zeros((4, 4), dtype=np.float32)
+
+        Mg2i = [
+            (fu*c2 + cu*c1*s2,       cu*c1*c2 - s2*fu,       -cu*s1,                 0),
+            (s2*(cv*c1 - fv*s1),     c2*(cv*c1 - fv*s1),     -fv*c1 - cv*s1,         0),
+            (c1*s2,                  c1*c2,                  -s1,                    0),
+            (c1*s2,                  c1*c2,                  -s1,                    0),
+        ]
+
+        self.program['uMg2i'] = Mg2i
+
+        return Mg2i
+
+    def on_draw(self, event):
+        self.program.draw()
+
+    def on_resize(self, event):
+        gloo.set_viewport(0, 0, *event.physical_size)
+        self.program['iResolution'] = (self.physical_size[0], self.physical_size[1], 0.)
 
 def parse_cmdline(parser):
-	parser.add_argument('-v', '--verbose', action='store_const', const=logging.DEBUG, default=logging.INFO, help='Be verbose...')
-	parser.add_argument('-i', '--image', help='Image file')
+    parser.add_argument('-v', '--verbose', action='store_const', const=logging.DEBUG, default=logging.INFO, help='Be verbose...')
+    parser.add_argument('-i', '--image', help='Image file')
+    parser.add_argument('-c', '--config', default='ipm.conf', help='IPM configuration file')
 
-	return parser.parse_args()
-
-# Some api in the chain is translating the keystrokes to this octal string
-# so instead of saying: ESCAPE = 27, we use the following.
-ESCAPE = '\033'
-
-
-class Texture( object ):
-	"""Texture either loaded from a file."""
-	def __init__( self ):
-		self.xSize, self.ySize = 0, 0
-		self.rawRefence = None
-
-class FileTexture(Texture):
-	"""Texture loaded from a file."""
-	def __init__( self, fileName ):
-		im = Image.open(fileName)
-		self.xSize = im.size[0]
-		self.ySize = im.size[1]
-		self.rawReference=np.array(list(im.getdata()),np.uint8)
-
-class ShaderProgram ( object ):
-	"""Manage GLSL programs."""
-	def __init__( self ):
-		self.__requiredExtensions = ["GL_ARB_fragment_shader",
-				 "GL_ARB_vertex_shader",
-				 "GL_ARB_shader_objects",
-				 "GL_ARB_shading_language_100",
-				 "GL_ARB_vertex_shader",
-				 "GL_ARB_fragment_shader"]
-		self.checkExtensions( self.__requiredExtensions )
-		self.__shaderProgramID = glCreateProgramObjectARB()
-		self.__checkOpenGLError()
-		self.__programReady = False
-		self.__isEnabled = False
-		self.__shaderObjectList = []
-
-	def checkExtensions( self, extensions ):
-		"""Check if all extensions in a list are present."""
-		for ext in extensions:
-			if ( not ext ):
-				print("Driver does not support %s", ext)
-				sys.exit()
-
-	def __checkOpenGLError( self ):
-		"""Print OpenGL error message."""
-		err = glGetError()
-		if ( err != GL_NO_ERROR ):
-			print('GLERROR: %s', gluErrorString( err ))
-			sys.exit()
-
-	def reset( self ):
-		"""Disable and remove all shader programs"""
-		for shaderID in self.__shaderObjectList:
-			glDetachObjectARB( self.__shaderProgramID, shaderID )
-			glDeleteObjectARB( shaderID )
-			self.__shaderObjectList.remove( shaderID )
-			self.__checkOpenGLError( )
-		glDeleteObjectARB( self.__shaderProgramID )
-		self.__checkOpenGLError( )
-		self.__shaderProgramID = glCreateProgramObjectARB()
-		self.__checkOpenGLError( )
-		self.__programReady = False
-
-	def addShader( self, shaderType, fileName ):
-		"""Read a shader program from a file.
-
-		The program is load and compiled"""
-		shaderHandle = glCreateShaderObjectARB( shaderType )
-		self.__checkOpenGLError( )
-		sourceString = open(fileName, 'r').read()
-		glShaderSourceARB(shaderHandle, [sourceString] )
-		self.__checkOpenGLError( )
-		glCompileShaderARB( shaderHandle )
-		success = glGetObjectParameterivARB( shaderHandle,
-				GL_OBJECT_COMPILE_STATUS_ARB)
-		if (not success):
-			print(glGetInfoLogARB( shaderHandle ))
-			sys.exit( )
-		glAttachObjectARB( self.__shaderProgramID, shaderHandle )
-		self.__checkOpenGLError( )
-		self.__shaderObjectList.append( shaderHandle )
-
-	def linkShaders( self ):
-		"""Link compiled shader programs."""
-		glLinkProgramARB( self.__shaderProgramID )
-		self.__checkOpenGLError( )
-		success = glGetObjectParameterivARB( self.__shaderProgramID,
-				GL_OBJECT_LINK_STATUS_ARB )
-		if (not success):
-			print(glGetInfoLogARB(self.__shaderProgramID))
-			sys.exit()
-		else:
-			self.__programReady = True
-
-	def enable( self ):
-		"""Activate shader programs."""
-		if self.__programReady:
-			glUseProgramObjectARB( self.__shaderProgramID )
-			self.__isEnabled=True
-			self.__checkOpenGLError( )
-		else:
-			print("Shaders not compiled/linked properly, enable() failed")
-
-	def disable( self ):
-		"""De-activate shader programs."""
-		glUseProgramObjectARB( 0 )
-		self.__isEnabled=False
-		self.__checkOpenGLError( )
-
-	def indexOfUniformVariable( self, variableName ):
-		"""Find the index of a uniform variable."""
-		if not self.__programReady:
-			print("\nShaders not compiled/linked properly")
-			result = -1
-		else:
-			result = glGetUniformLocationARB( self.__shaderProgramID, variableName)
-			self.__checkOpenGLError( )
-		if result < 0:
-			print('Variable "%s" not known to the shader', ( variableName ))
-			sys.exit( )
-		else:
-			return result
-
-	def indexOfVertexAttribute( self, attributeName ):
-		"""Find the index of an attribute variable."""
-		if not self.__programReady:
-			print("\nShaders not compiled/linked properly")
-			result = -1
-		else:
-			result = glGetAttribLocationARB( self.__shaderProgramID, attributeName )
-			self.__checkOpenGLError( )
-		if result < 0:
-			print('Attribute "%s" not known to the shader', ( attributeName ))
-			sys.exit( )
-		else:
-			return result
-
-	def isEnabled( self ):
-		return self.__isEnabled
-
-	def getShaderProgId(self):
-		return self.__shaderProgramID
-
-# The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)
-def keyPressed(*args):
-	global window
-	# If escape is pressed, kill everything.
-	if args[0].decode() == ESCAPE:
-		sys.exit()
-
-def display():
-	"""Glut display function."""
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-	glColor3f(1.0, 0.0, 0.0)
-	glBegin(GL_QUADS)
-#	glTexCoord2f( 0, 1 )
-	glVertex3f(-0.5, -0.5, 0)
-#	glTexCoord2f( 0, 0 )
-	glVertex3f(-0.5, +0.5, 0)
-#	glTexCoord2f( 1, 0 )
-	glVertex3f(0.5, +0.5, 0)
-#	glTexCoord2f( 1, 1 )
-	glVertex3f(0.5, -0.5, 0)
-	glEnd()
-	glutSwapBuffers()
-
-def initTexture(fileName):
-	"""Glut init function."""
-	try:
-		texture = FileTexture(fileName)
-	except:
-		print('could not open ', fileName, '; using random texture')
-		return
-
-	glClearColor(0, 0, 0, 0)
-	glShadeModel(GL_SMOOTH)
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-	glTexImage2D(GL_TEXTURE_2D, 0, 3, texture.xSize, texture.ySize, 0,
-							 GL_RGB, GL_UNSIGNED_BYTE, texture.rawReference)
-	glEnable(GL_TEXTURE_2D)
-
-def initShaders():
-	"""Initialise shaderProg object."""
-	global sP
-	sP = ShaderProgram( )
-	sP.addShader(GL_FRAGMENT_SHADER_ARB, join(dirname(realpath(__file__)),'ipm.frag'))
-	glBindFragDataLocation(sP.getShaderProgId(), 0, 'fragColor')
-
-	sP.linkShaders( )
-
-	sP.enable( )
-
-	glUniform1fARB( sP.indexOfUniformVariable("pitch"), 14.0 * math.pi / 180)
-	glUniform1fARB( sP.indexOfUniformVariable("yaw"), 0 * math.pi / 180)
-	glUniform1fARB( sP.indexOfUniformVariable("fu"), 309.4362)
-	glUniform1fARB( sP.indexOfUniformVariable("fv"), 344.2161)
-	glUniform1fARB( sP.indexOfUniformVariable("cu"), 317.9034)
-	glUniform1fARB( sP.indexOfUniformVariable("cv"), 256.5352)
-	glUniform1fARB( sP.indexOfUniformVariable("h"), 2179.8)
-
-	glUniform2fvARB( sP.indexOfUniformVariable("iResolution"), 1, (640, 480))
-	glUniformMatrix2fvARB(sP.indexOfUniformVariable("wROI"), 1, GL_FALSE, ((-11048.41, 3933.21), (7204.76,15698.30)))
-	glUniformMatrix2fvARB(sP.indexOfUniformVariable("iROI"), 1, GL_FALSE, ((100, 220), (460, 350)))
+    return parser.parse_args()
 
 def main():
-	logging.basicConfig(format=FORMAT)
-	parser = ArgumentParser(description= "Apply an Inverse Perspective Mapping on a img")
-	args = parse_cmdline(parser)
-	log = logging.getLogger("ipm openGL")
-	log.setLevel(args.verbose)
-	log.info("OpenGL acceleration to compute an IPM")
+    vispy.set_log_level('DEBUG')
+    try:
+        vispy.use(app='glfw', gl='gl+')
+    except RuntimeError as e:
+        pass
 
-	glutInit(sys.argv)
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
-	glutInitWindowSize(250, 250)
-	glutInitWindowPosition(100, 100)
-	glutCreateWindow(sys.argv[0])
-	glutKeyboardFunc(keyPressed)
-	glutDisplayFunc( display )
+    logging.basicConfig(format=FORMAT)
+    parser = ArgumentParser(description= "Apply an Inverse Perspective Mapping on a img")
+    args = parse_cmdline(parser)
+    log = logging.getLogger("ipm openGL")
+    log.setLevel(args.verbose)
+    log.info("OpenGL acceleration to compute an IPM")
 
+    config = configparser.ConfigParser(inline_comment_prefixes=('#'))
+    try:
+        config.read(args.config)
+    except:
+        log.fatal("Incorrect config file!")
+        sys.exit()
 
-	initTexture(args.image)
-	initShaders()
+    conf = {}
 
-	glutMainLoop(  )
+    if config.has_section('camera'):
+        conf['yaw'] = config.getfloat('camera', 'yaw') * np.pi / 180
+        conf['pitch'] = config.getfloat('camera', 'pitch') * np.pi / 180
+        conf['c1'] = np.cos(conf['pitch'])
+        conf['s1'] = np.sin(conf['pitch'])
+        conf['c2'] = np.cos(conf['yaw'])
+        conf['s2'] = np.sin(conf['yaw'])
+        conf['fu'] = config.getfloat('camera', 'focalLengthX')
+        conf['fv'] = config.getfloat('camera', 'focalLengthY')
+        conf['cu'] = config.getfloat('camera', 'opticalCenterX')
+        conf['cv'] = config.getfloat('camera', 'opticalCenterY')
+        conf['h'] = config.getfloat('camera', 'cameraHeight')
+    else:
+        log.fatal("Configuration file must have a camera section!")
+        sys.exit()
 
-	# Register the function called when the keyboard is pressed.
+    if config.has_section('ipm'):
+        conf['ipmWidth']  = config.getint('ipm', 'ipmWidth')
+        conf['ipmHeight']  = config.getint('ipm', 'ipmHeight')
+        conf['ipmTop']  = config.getint('ipm', 'ipmTop')
+        conf['ipmBottom'] = config.getint('ipm', 'ipmBottom')
+        conf['ipmLeft'] = config.getint('ipm', 'ipmLeft')
+        conf['ipmRight'] = config.getint('ipm', 'ipmRight')
+        conf['ipmInterpolation'] = config.getint('ipm', 'ipmInterpolation')
+    else:
+        log.fatal("Configuration file must have a IPM section!")
+        sys.exit()
+
+    c = IpmCanvas(args.image, conf)
+    app.run()
 
 print("Hit ESC key to quit.")
 main()
